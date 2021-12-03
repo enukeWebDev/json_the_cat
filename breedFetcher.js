3d734b73962998f35057a0f5709e98d1936d9542
Roderick Nuque <enuke4@gmail.com>
2021-12-03 02:26:30 +0000

    first commit for this exercise

---
+++ breedFetcher.js
@@ -0,0 +1,23 @@
+const request = require('request');
+
+const fetchCatBreedInformation = (catsBreed, callbackFunc) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${catsBreed}`, (error, message, body) => {
+    //Perfect Case - no error
+    if (!error) {
+      let information = JSON.parse(body);
+      //console.log(information);
+
+      //Edge Case - Breed not found
+      if (information.length === 0) {
+        return callbackFunc(`Sorry! The Breed does not have a match...`, null);
+      } else {
+        return callbackFunc(error, information[0].description);
+      }
+
+    } else {
+      return callbackFunc(error, 'Opppsss...Try again!');
+    }
+  });
+};
+
+module.exports = { fetchCatBreedInformation };
